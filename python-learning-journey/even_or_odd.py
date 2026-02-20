f287c337b7c803127bbc5ba992bb184fa1d83e52
Sreeshma <babyshreeshma@gmail.com>
2026-02-20 18:29:10 +0530

    wrote program for multiple conitions

--- python-learning-journey/even_or_odd.py
+++ python-learning-journey/even_or_odd.py
@@ -1,6 +1,22 @@
+#even or odd
+
+
 number=int(input("Enter a number to check odd or even: "))
 if(number%2==0):
     print(f"{number} is an even number")
 else:
     print(f"{number} is an odd number")
 print("newly added line")
+
+"""
+multiple conditions
+"""
+mark=int(input("Enter mark: "))
+if(mark>=90):
+    print("Grade A")
+elif(mark>=80 and mark<90):
+    print("Grade B")
+elif(mark>=70 and mark<60):
+    print("Grade C")
+else:
+    Print("Grade P")
