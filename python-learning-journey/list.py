f2b03d75c063a19b406f7925da2e62dbfc8400a8
Sreeshma <babyshreeshma@gmail.com>
2026-02-22 11:20:27 +0530

    updated list

--- python-learning-journey/list.py
+++ python-learning-journey/list.py
@@ -5,3 +5,13 @@ print(numbers)
 #Access list
 print(numbers[0])
 print(numbers[1])
+
+#Loop through a list
+for num in numbers:
+    print(num)
+
+#add and remove item
+numbers.append(60)
+numbers.remove(10)
+
+print(numbers)
